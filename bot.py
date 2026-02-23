09726a299731222d2f575fc15bd64fc6ae14d5c6
bygeldi <bygeldi3421@gmail.com>
2026-02-24 00:06:11 +0300

    Bot artik nasilsin ve kimsin sorularina cevap veriyor

--- bot.py
+++ bot.py
@@ -1,2 +1,14 @@
 print("Bot calisiyor...")
 input("Mesajiniz nedir?: ")
+def cevap_ver(mesaj):
+    mesaj = mesaj.lower()
+    if "nasılsın" in mesaj:
+        return "Bot olduğum için her zaman enerjik hissediyorum! Sen?"
+    elif "kimsin" in mesaj:
+        return "Ben senin yerime mesajlara bakan akıllı botunum."
+    else:
+        return "Bunu henüz öğrenmedim ama üzerinde çalışıyorum."
+
+# Kullanıcıdan mesaj alalım
+gelen = input("Gelen Mesaj: ")
+print("Botun Cevabı:", cevap_ver(gelen))
